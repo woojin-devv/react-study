6cb9bad210e1159df343b8498c0f396172890fe8
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-01-11 20:36:01 +0900

    refactor: update dependencies and integrate Tailwind CSS for styling
    - Updated package-lock.json and package.json to include @tailwindcss/postcss.
    - Modified postcss.config.js to use '@tailwindcss/postcss' plugin.
    - Refactored Header component to use Tailwind CSS classes instead of styled-components.
    - Cleaned up import statements in App.jsx for consistency.

--- 06-styling-react-component/src/components/Header.jsx
+++ 06-styling-react-component/src/components/Header.jsx
@@ -1,58 +1,70 @@
-import { styled } from "styled-components";
-
 import logo from "../assets/logo.png";
 
-const StyleHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
+// styled-components 패키지 사용
+// 1. import
+// import { styled } from "styled-components";
 
+// 2. 정의
+// const StyleHeader = styled.header`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   margin-top: 2rem;
+//   margin-bottom: 2rem;
 
-& img {
-  object-fit: contain;
-  margin-bottom: 2rem;
-  width: 11rem;
-  height: 11rem;
-}
+// & img {
+//   object-fit: contain;
+//   margin-bottom: 2rem;
+//   width: 11rem;
+//   height: 11rem;
+// }
 
-&  h1 {
-  font-size: 1.5rem;
-  font-weight: 600;
-  letter-spacing: 0.4em;
-  text-align: center;
-  text-transform: uppercase;
-  color: #9a3412;
-  font-family: 'Pacifico', cursive;
-  margin: 0;
-}
+// &  h1 {
+//   font-size: 1.5rem;
+//   font-weight: 600;
+//   letter-spacing: 0.4em;
+//   text-align: center;
+//   text-transform: uppercase;
+//   color: #9a3412;
+//   font-family: 'Pacifico', cursive;
+//   margin: 0;
+// }
 
-& p {
-  text-align: center;
-  color: #a39191;
-  margin: 0;
-}
+// & p {
+//   text-align: center;
+//   color: #a39191;
+//   margin: 0;
+// }
 
-@media (min-width: 768px) {
-  margin-bottom: 4rem;
+// @media (min-width: 768px) {
+//   margin-bottom: 4rem;
 
-  & h1 {
-    font-size: 2.25rem;
-  }
-}
+//   & h1 {
+//     font-size: 2.25rem;
+//   }
+// }
+// `
 
+//3. 사용
+// export default function Header() {
+//   return (
+//     <StyleHeader>
+//       <img src={logo} alt="A canvas" />
+//       <h1>ReactArt</h1>
+//       <p>
+//         A community of artists and art-lovers.
+//       </p>
+//     </StyleHeader>
+//   );
+// }
 
-`
 export default function Header() {
   return (
-    <StyleHeader>
+    <header className="flex flex-col items-cente mt">
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>
-        A community of artists and art-lovers.
-      </p>
-    </StyleHeader>
+      <p>A community of artists and art-lovers.</p>
+    </header>
   );
 }
